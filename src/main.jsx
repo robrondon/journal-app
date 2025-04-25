b48bc45cb7a9a976c5d2f0bb28678085e36a73a6
Robert Rondon <robrondon11@gmail.com>
2025-04-25 17:06:12 -0300

    Material UI configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
 import './styles.css'
 import { JournalApp } from './JournalApp'
-import { BrowserRouter } from 'react-router'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
