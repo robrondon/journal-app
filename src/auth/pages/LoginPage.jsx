bd0e7c90a38975974ca2577b5e3c8966eeb03af8
Robert Rondon <robrondon11@gmail.com>
2025-04-26 15:01:26 -0300

    AuthLayout and LoginPage structure

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,5 +1,73 @@
+import { Link as RouterLink } from 'react-router'
+import { Google } from '@mui/icons-material'
+import { Button, Grid, Link, TextField, Typography } from '@mui/material'
+import { AuthLayout } from '../layout/AuthLayout'
+
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <AuthLayout title='Login'>
+      <form>
+        <Grid container>
+          <Grid
+            size={{ xs: 12 }}
+            sx={{ marginTop: 2 }}
+          >
+            <TextField
+              label="Email"
+              type='email'
+              placeholder='email@email.com'
+              fullWidth
+            />
+          </Grid>
+
+          <Grid
+            size={{ xs: 12 }}
+            sx={{ marginTop: 2 }}
+          >
+            <TextField
+              label="Password"
+              type='password'
+              placeholder='Password'
+              fullWidth
+            />
+          </Grid>
+
+          <Grid
+            container
+            size={12}
+            spacing={2}
+            sx={{ mb: 2, mt: 2 }}
+          >
+            <Grid size={{ xs: 12, sm: 6 }}>
+              <Button variant='contained' fullWidth>
+                <Typography sx={{ ml: 1 }}> Login </Typography>
+              </Button>
+            </Grid>
+
+            <Grid size={{ xs: 12, sm: 6 }}>
+              <Button variant='contained' fullWidth>
+                <Google />
+                <Typography sx={{ ml: 1 }}> Google </Typography>
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid
+            container
+            size={12}
+            direction='row'
+            justifyContent='end'
+          >
+            <Link
+              color='inherit'
+              to={'/auth/register'}
+              component={RouterLink}
+            >
+              Crear una cuenta
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
   )
 }
