c561706b784ce34290f138e029654a755f3be180
Robert Rondon <robrondon11@gmail.com>
2025-04-27 14:05:52 -0300

    NothingSelectedView structure

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,11 @@
 import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam officia molestias commodi consectetur, culpa fugit. Quis consequuntur assumenda nostrum mollitia expedita enim esse qui, nam atque, accusantium praesentium in labore!</Typography>
-      {/* NothingSelected */}
+      <NothingSelectedView />
       {/* NoteView */}
     </JournalLayout>
   )
