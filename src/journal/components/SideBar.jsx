e77851dc941403265c0cc789a5dc056744c3aef0
Robert Rondon <robrondon11@gmail.com>
2025-04-27 13:46:48 -0300

    SideBar component

---
+++ src/journal/components/SideBar.jsx
@@ -0,0 +1,43 @@
+import { TurnedInNot } from "@mui/icons-material"
+import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
+
+export const SideBar = ({ drawerWidth = 240 }) => {
+  return (
+    <Box
+      component='nav'
+      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+    >
+      <Drawer
+        variant='permanent' // temporary
+        open
+        sx={{
+          display: { xs: 'block' },
+          '& .MuiDrawer-paper': {
+            boxSizing: 'border-box',
+            width: drawerWidth
+          }
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component='div'> Rob Rondon </Typography>
+        </Toolbar>
+        <Divider />
+        <List>
+          {['January', 'February', 'March', 'April'].map(text => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <TurnedInNot />
+                </ListItemIcon>
+                <Grid container>
+                  <ListItemText primary={text} />
+                  <ListItemText secondary={'Lorem ipsum dolor sit amet consectetur adipisicing elit.'} />
+                </Grid>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  )
+}
--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,16 +1,15 @@
-import { Box } from "@mui/material"
-import { NavBar } from "../components"
+import { Box, Toolbar } from "@mui/material"
+import { NavBar, SideBar } from "../components"
 
 const drawerWidth = 240
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex' }}>
-      {/* Navbar drawerWidth */}
       <NavBar drawerWidth={drawerWidth} />
-      {/* Sidebar drawerWidth */}
+      <SideBar drawerWidth={drawerWidth} />
       <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
-        {/* Toolbar */}
+        <Toolbar />
         {children}
       </Box>
     </Box>
