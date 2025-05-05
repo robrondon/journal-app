a3c9f9a7d744678ddc98350688e0b6d74c2942d6
Robert Rondon <robrondon11@gmail.com>
2025-05-05 19:00:14 -0300

    Google SignIn providers

---
+++ src/firebase/providers.js
@@ -0,0 +1,31 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { FirebaseAuth } from "./config";
+
+const googleProvider = new GoogleAuthProvider()
+
+export const signInWithGoogle = async () => {
+  try {
+    const result = await signInWithPopup(FirebaseAuth, googleProvider)
+    // This is useful if you need to get google access tokens etc  
+    // const credentials = GoogleAuthProvider.credentialFromResult(result)
+    // console.log(credentials)
+    const { displayName, email, photoURL, uid } = result.user
+
+    return {
+      ok: true,
+      displayName,
+      email,
+      photoURL,
+      uid
+    }
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+
+    return {
+      ok: false,
+      errorCode,
+      errorMessage
+    }
+  }
+}
