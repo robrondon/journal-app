9efe2b635bc55fbee0649362f6ac6b6d6fd03a24
Robert Rondon <robrondon11@gmail.com>
2025-05-07 19:25:42 -0300

    Updating useForm to manage validations and touchedFields

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,24 +1,62 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
-export const useForm = (initialForm = {}) => {
+export const useForm = (initialForm = {}, formValidations = {}) => {
   const [formState, setFormState] = useState(initialForm);
+  const [formErrors, setFormErrors] = useState({});
+  const [isFormValid, setIsFormValid] = useState(false);
+  const [touchedFields, setTouchedFields] = useState({});
+
+  const validateForm = useCallback(() => {
+    const errors = {}
+    let valid = true
+
+    for (const field in formValidations) {
+      const validations = formValidations[field]
+      for (const [fn, errorMessage] of validations) {
+        const value = formState[field] || ''
+        const isValid = fn(value)
+        if (!isValid) {
+          errors[field] = errorMessage
+          valid = false
+          break
+        }
+      }
+    }
+
+    setFormErrors(errors);
+    setIsFormValid(valid);
+
+  }, [formState, formValidations])
 
   const onInputChange = ({ target }) => {
     const { name, value } = target
-    setFormState({
-      ...formState,
-      [name]: value
-    })
+    setFormState(prev => ({ ...prev, [name]: value }));
   }
+
+  const onInputBlur = ({ target }) => {
+    const { name } = target;
+    setTouchedFields(prev => ({ ...prev, [name]: true }));
+  };
 
   const onResetForm = () => {
     setFormState(initialForm)
+    setFormErrors({});
+    setIsFormValid(false);
+    setTouchedFields({});
   }
+
+  useEffect(() => {
+    validateForm()
+  }, [formState, validateForm]);
 
   return {
     ...formState,
+    formErrors,
     formState,
+    isFormValid,
+    onInputBlur,
     onInputChange,
-    onResetForm
+    onResetForm,
+    touchedFields
   }
 }
