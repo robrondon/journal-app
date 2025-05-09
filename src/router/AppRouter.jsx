12aea04bf302cd407cf1f2206a379808e2a55a4e
Robert Rondon <robrondon11@gmail.com>
2025-05-09 18:04:07 -0300

    Adding loader before routes to check if user is authenticated

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,14 @@
+import { useSelector } from "react-redux"
 import { Route, Routes } from "react-router"
 import { AuthRoutes } from "../auth"
 import { JournalRoutes } from "../journal"
+import { CheckingAuth } from "../ui"
 
 export const AppRouter = () => {
+  const { status } = useSelector(state => state.auth)
+
+  if (status === 'checking') return <CheckingAuth />
+
   return (
     <Routes>
       <Route path="/auth/*" element={<AuthRoutes />} />
