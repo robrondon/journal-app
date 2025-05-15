9b49a8765639f696c7a1dc4fc96a0fad2abcca40
Robert Rondon <robrondon11@gmail.com>
2025-05-15 15:07:48 -0300

    Moved initialForm values to a variable to avoid infite re-renders

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -7,14 +7,16 @@ import { AuthLayout } from '../layout/AuthLayout'
 import { useForm } from '../../hooks'
 import { startGoogleSignIn, startUserLogin } from '../../store/auth'
 
+const formData = {
+  email: '',
+  password: ''
+}
+
 export const LoginPage = () => {
   const { status, errorMessage } = useSelector(state => state.auth)
   const dispatch = useDispatch()
   const [formSubmitted, setFormSubmitted] = useState(false);
-  const { email, password, onInputChange } = useForm({
-    email: '',
-    password: ''
-  })
+  const { email, password, onInputChange } = useForm(formData)
 
   const isAuthenticating = useMemo(() => status === 'checking', [status])
 
