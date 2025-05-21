a16f9f1be850c50c5e1c6378fa8a2862a02bd57e
Robert Rondon <robrondon11@gmail.com>
2025-05-21 17:58:27 -0300

    Cleaning states in login page

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -15,22 +15,18 @@ const formData = {
 export const LoginPage = () => {
   const { status, errorMessage } = useSelector(state => state.auth)
   const dispatch = useDispatch()
-  const [formSubmitted, setFormSubmitted] = useState(false);
   const { email, password, onInputChange } = useForm(formData)
 
   const isAuthenticating = useMemo(() => status === 'checking', [status])
 
   const onSubmit = (e) => {
     e.preventDefault()
-    setFormSubmitted(true)
-    console.log('que onda?')
     dispatch(startUserLogin({ email, password }))
   }
 
   const onGoogleSignIn = () => {
     dispatch(startGoogleSignIn())
   }
-  console.log({ formSubmitted, errorMessage })
 
   return (
     <AuthLayout title='Login'>
